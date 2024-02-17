906a21f3da3a5fd2266edab3b409fca9f818374e
parth adhikari <parthadhikari1812@gmail.com>
2024-02-17 21:39:26 +0530

    added variants

--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import {motion} from "framer-motion"
+
+const containerVariant = {
+  hidden:{
+    x:'100vw'
+  },
+  visible:{
+    x:0,
+    transition:{
+      delay:0.3,
+      type:'spring',
+      stiffness:120
+    }
+  }
+}
 
 const Order = ({ pizza }) => {
   return (
-    <div className="container order">
+    <motion.div className="container order"
+      variants={containerVariant}
+      initial='hidden'
+      animate='visible'
+    >
       <h2>Thank you for your order :)</h2>
       <p>You ordered a {pizza.base} pizza with:</p>
       {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
+    </motion.div>
   )
 }
 
