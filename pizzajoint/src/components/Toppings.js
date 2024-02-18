77188987028b1131d7ebc6e3c434729e7dc9517e
parth adhikari <parthadhikari1812@gmail.com>
2024-02-18 23:52:00 +0530

    added exit animations

--- pizzajoint/src/components/Toppings.js
+++ pizzajoint/src/components/Toppings.js
@@ -1,59 +1,83 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {motion} from "framer-motion"
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const containerVariant = {
-  hidden:{
-    x:'100vw'
+  hidden: {
+    x: "100vw",
   },
-  visible:{
-    x:0,
-    transition:{
-      delay:0.3,
-      type:'spring',
-      stiffness:120
-    }
-  }
-}
+  visible: {
+    x: 0,
+    transition: {
+      delay: 0.3,
+      type: "spring",
+      stiffness: 120,
+    },
+  },
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
+  },
+};
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
 
 const Toppings = ({ addTopping, pizza }) => {
-  let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
+  let toppings = [
+    "mushrooms",
+    "peppers",
+    "onions",
+    "olives",
+    "extra cheese",
+    "tomatoes",
+  ];
 
   return (
-    <motion.div className="toppings container"
+    <motion.div
+      className="toppings container"
       variants={containerVariant}
-      initial='hidden'
-      animate='visible'
+      initial="hidden"
+      animate="visible"
+      exit="exit"
     >
-      
       <h3>Step 2: Choose Toppings</h3>
       <ul>
-        {toppings.map(topping => {
-          let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
+        {toppings.map((topping) => {
+          let spanClass = pizza.toppings.includes(topping) ? "active" : "";
           return (
-            <motion.li key={topping} onClick={() => addTopping(topping)}
-              whileHover={{type: 'spring',stiffness: 300, scale: 1.3, color: '#f8e112', originX:0}}
+            <motion.li
+              key={topping}
+              onClick={() => addTopping(topping)}
+              whileHover={{
+                type: "spring",
+                stiffness: 300,
+                scale: 1.3,
+                color: "#f8e112",
+                originX: 0,
+              }}
             >
-              <span className={spanClass}>{ topping }</span>
+              <span className={spanClass}>{topping}</span>
             </motion.li>
-          )
+          );
         })}
       </ul>
 
       <Link to="/order">
-        <motion.button
-          whileHover={{
-            scale:1.1,
-            textShadow: "0px 0px 8px rgb(255,255,255)",
-            boxShadow: "0px 0px 8px rgb(255,255,255)",
-          }}
-        >
+        <motion.button variants={buttonVariants} whileHover="hover">
           Order
         </motion.button>
       </Link>
-
     </motion.div>
-  )
-}
+  );
+};
 
 export default Toppings;
