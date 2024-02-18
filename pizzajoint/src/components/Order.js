354fc22bda1e3c614c650c2731d87ff8a52e606e
parth adhikari <parthadhikari1812@gmail.com>
2024-02-18 21:06:15 +0530

    added variants

--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -1,32 +1,50 @@
-import React from 'react';
-import {motion} from "framer-motion"
+import React from "react";
+import { motion } from "framer-motion";
 
 const containerVariant = {
-  hidden:{
-    x:'100vw'
+  hidden: {
+    x: "100vw",
   },
-  visible:{
-    x:0,
-    transition:{
-      delay:0.3,
-      type:'spring',
-      stiffness:120
-    }
-  }
-}
+  visible: {
+    x: 0,
+    transition: {
+      type: "spring",
+      mass: 0.4, // more mass it moves slower
+      damping: 8, // higher no. means less oscillations
+      when: "beforeChildren",
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const childrenVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
 
 const Order = ({ pizza }) => {
   return (
-    <motion.div className="container order"
+    <motion.div
+      className="container order"
       variants={containerVariant}
-      initial='hidden'
-      animate='visible'
+      initial="hidden"
+      animate="visible"
     >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
+      <motion.p variants={childrenVariant}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childrenVariant}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
     </motion.div>
-  )
-}
+  );
+};
 
 export default Order;
