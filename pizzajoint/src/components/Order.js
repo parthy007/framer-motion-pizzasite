c341a10156508f657a3e3c3835b883afb0e5d05d
parth adhikari <parthadhikari1812@gmail.com>
2024-02-19 20:15:27 +0530

    added triggering of backdrop

--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 
 const containerVariant = {
@@ -26,7 +26,13 @@ const childrenVariant = {
   },
 };
 
-const Order = ({ pizza }) => {
+const Order = ({ pizza, setShowModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true);
+    }, 5000);
+  }, [setShowModal]);
+
   return (
     <motion.div
       className="container order"
